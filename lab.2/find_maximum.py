0b5a66f67f3bb70ecf79107e39ac36fa8d0b4657
TheSentry-ctrl <B231960016@gmail.com>
2025-12-01 14:29:21 +0800

    Update find_maximum.py

--- lab.2/find_maximum.py
+++ lab.2/find_maximum.py
@@ -5,4 +5,6 @@ class FindMaximum:
         mid = (low + high) // 2
         left_max = self.find_max(array, low, mid)
         right_max = self.find_max(array, mid + 1, high)
+
+        
         return max(left_max, right_max)
